946486b5b6f6b9b4d555ba87992713ec445f4bf0
Artem Agvanian <artemagvanian@icloud.com>
2023-02-05 21:55:17 -0500

    feat: implement Bohm's heuristic

---
+++ src/strategies/branching/BohmsStrategy.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "strategies/branching/BranchingStrategy.h"
+
+class BohmsStrategy : public BranchingStrategy {
+private:
+    long alpha;
+    long beta;
+public:
+    BohmsStrategy(long alpha, long beta) {
+        this->alpha = alpha;
+        this->beta = beta;
+    }
+
+    std::pair<Variable, LiteralValue> choose(const Formula &initial) const override;
+};
